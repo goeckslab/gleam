c7d7fe93c411e172151693f293e80993af6a78d1
paulocilasjr <pj_ibm@hotmail.com>
2025-07-02 23:32:58 -0400

    quotes change

--- tools/imagelearner/image_learner_cli.py
+++ tools/imagelearner/image_learner_cli.py
@@ -43,7 +43,7 @@ from utils import (
 # --- Logging Setup ---
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    format='%(asctime)s %(levelname)s %(name)s: %(message)s',
 )
 logger = logging.getLogger("ImageLearner")
 
